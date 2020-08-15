5161379499083e26c4d47e0095242eb063b5467f
Prathamesh Jadhav <prathameshjc@gmail.com>
2020-08-16 04:21:21 +0530

    build

--- Server.js
+++ Server.js
@@ -9,7 +9,7 @@ function respond(req, res, next) {
 
 server.get('/hello', respond);
 server.get(/\/?.*/, restify.plugins.serveStatic({
-  directory: './build/client',
+  directory: './build',
   default: 'index.html'
 }))
 // server.get('/*',restify.plugins.serveStatic({
