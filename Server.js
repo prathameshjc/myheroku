17712ff9ebeab0bf845ba7be48d340ed5efaf156
Prathamesh Jadhav <prathameshjc@gmail.com>
2020-08-16 17:07:15 +0530

    build created

--- Server.js
+++ Server.js
@@ -8,42 +8,12 @@ function respond(req, res, next) {
 }
 
 server.get('/hello', respond);
-// server.get(/\/?.*/, restify.plugins.serveStatic({
-//   directory: './build',
-//   default: 'index.html'
-// }))
 server.get('/*',restify.plugins.serveStatic({
   directory:'./build',
   default:'index.html'
 }))
-// server.get(/\/?.*/, restify.plugins.serveStatic({
-//   directory: '../build'
-// }))
-// server.get("*", restify.plugins.serveStatic({
-//   directory: __dirname+"/client/build",
-//   default: 'index.html',
-//   appendRequestPath: false
-//  })
-// );
-// server.pre(serve_static(path.join(__dirname, '..', 'build')))
+
 var port = process.env.PORT || 5000;
 server.listen(port, function() {
     console.log("Listening on " + port);
 });
-//................express Server.......................
-// const express = require('express');
-// const favicon = require('express-favicon');
-// const path = require('path');
-// const port = process.env.PORT || 8080;
-// const app = express();
-// app.use(favicon(__dirname + '/build/favicon.ico'));
-// // the __dirname is the current directory from where the script is running
-// app.use(express.static(__dirname));
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.get('/ping', function (req, res) {
-//  return res.send('pong');
-// });
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-// app.listen(port);
--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Hrlogin from './component/Hrlogin'
-// import logo from './logo.svg';
-// import './App.css';
+
 
 function App() {
   return (
